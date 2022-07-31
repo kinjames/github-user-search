ef44379534494186966344926cebd5ddcac97fac
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-07-31 15:58:46 +0000

    Add files via upload

--- apps/js/script.js
+++ apps/js/script.js
@@ -11,3 +11,44 @@
 // });
 
 console.log('hello world');
+let avatar = document.getElementById('avatar');
+let mainName = document.getElementById('mainName');
+let handle = document.getElementById('handle');
+let dateJoined = document.getElementById('dateJoined');
+let bio = document.getElementById('bio');
+let repos = document.getElementById('publicRepos');
+let followers = document.getElementById('followers');
+let following = document.getElementById('following');
+let locationText = document.getElementById('location');
+let urlText = document.getElementById('urlText');
+let twitter = document.getElementById('twitter');
+let company = document.getElementById('company');
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    var search = document.getElementById('search').value;
+
+    var normalName = search.split(' ').join('');
+
+
+    fetch("https://api.github.com/users/"+normalName)
+    .then((result) => result.json())
+    .then((data) => {
+        avatar.textContent = data.avatar_url;
+        mainName.textContent = data.name;
+        handle.textContent = "@"+ data.login;
+        dateJoined.textContent = data.created_at;
+        bio.textContent = data.bio;
+        repos.textContent = data.public_repos;
+        followers.textContent = data.followers;
+        following.textContent = data.following;
+        locationText.textContent = data.location;
+        urlText.textContent = data.url;
+        twitter.textContent = data.twitter_username;
+        company.textContent = data.company;
+    })
+
+})
